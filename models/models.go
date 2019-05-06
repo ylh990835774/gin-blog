230ad2dcd24397df60a91fa4c78d7eead7d53672
YlHunter <njyanglinghui@126.com>
2019-05-06 12:30:47 +0800

    use GORM Callbacks: Automatically populates the create/update time

--- models/models.go
+++ models/models.go
@@ -8,6 +8,7 @@ import (
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 
 	"gin-blog/pkg/setting"
+	"time"
 )
 
 var db *gorm.DB
@@ -53,8 +54,41 @@ func init() {
 	db.SingularTable(true)
 	db.DB().SetMaxIdleConns(10)
 	db.DB().SetMaxOpenConns(100)
+
+	db.Callback().Create().Replace("gorm:update_time_stamp", updateTimeStampForCreateCallback)
+	db.Callback().Update().Replace("gorm:update_time_stamp", updateTimeStampForUpdateCallback)
 }
 
 func CloseDB() {
 	defer db.Close()
+}
+
+func updateTimeStampForCreateCallback(scope *gorm.Scope) {
+	// 检查是否有含有错误（db.Error）
+	if !scope.HasError() {
+		nowTime := time.Now().Unix()
+		// scope.FieldByName 通过 scope.Fields() 获取所有字段，判断当前是否包含所需字段
+		if createTimeField, ok := scope.FieldByName("CreatedOn"); ok {
+			// field.IsBlank 可判断该字段的值是否为空
+			if createTimeField.IsBlank {
+				// field.Set 用于给该字段设置值，参数为 interface{}
+				createTimeField.Set(nowTime)
+			}
+		}
+
+		if modifyTimeField, ok := scope.FieldByName("ModifiedOn"); ok {
+			if modifyTimeField.IsBlank {
+				modifyTimeField.Set(nowTime)
+			}
+		}
+	}
+}
+
+func updateTimeStampForUpdateCallback(scope *gorm.Scope) {
+	// scope.Get(...) 根据入参获取设置了字面值的参数，
+	// 例如 gorm:update_column ，它会去查找含这个字面值的字段属性
+	// scope.SetColumn(...) 假设没有指定 update_column 的字段，我们默认在更新回调设置 ModifiedOn 的值
+	if _, ok := scope.Get("gorm:update_column"); !ok {
+		scope.SetColumn("ModifiedOn", time.Now().Unix())
+	}
 }
